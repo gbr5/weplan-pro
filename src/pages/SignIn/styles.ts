a0d99427b90df04ae7466f7519c9c48068e6d55f
gbr <gbrobbe@gmail.com>
2020-10-20 02:13:03 -0300

    Revert "Weplanners web/clean previous project"

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -2,264 +2,99 @@ import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import '../../styles/global';
 
-import signUpBackgroundImg from '../../assets/sign-up-background.jpeg';
+import signInBackgroundImg from '../../assets/signInImage1.jpg';
 
 export const Container = styled.div`
-  height: 100%;
-  width: 100%;
-  padding: 56px 40px 0;
-  @media (max-width: 800px) {
-    padding: 40px 0 0 24px;
-  }
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
 `;
 
 export const Content = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  background: transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   place-content: center;
 
   width: 100%;
-
-  @media (max-width: 800px) {
-    display: flex;
-  }
+  max-width: 700px;
 `;
 
-const appearFromDown = keyframes`
+const appearFromLeft = keyframes`
   from {
     opacity: 0;
-    transform: translateY(-480px);
+    transform: translateX(-80px);
   }
   to {
     opacity: 1;
-    transform: translateY(0);
+    transform: translateX(0);
   }
 `;
 
-export const ToggleButton = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  top: 8px;
-  left: 8px;
-  gap: 16px;
-
-  h3 {
-    font-size: 20px;
-    font-weight: 500px;
-    color: var(--title-color);
-  }
-
-  button {
-    background: transparent;
-    border: none;
-
-    h2 {
-      font-size: 20px;
-      font-weight: 500px;
-      color: var(--letter-color-3);
-      transition: 0.5s;
-      &:hover {
-        color: var(--primary-color);
-      }
-    }
-  }
-`;
 export const AnimationContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100%;
-  width: 100%;
+
   place-content: center;
-  gap: 16px;
 
-  animation: ${appearFromDown} 0.8s;
-
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 16px;
-    width: 100%;
-
-    h1 {
-      padding-top: 12px;
-      color: var(--title-color);
-      font-size: 32px;
-    }
-    h2 {
-      color: var(--letter-color-3);
-      font-size: 24px;
-      padding: auto 0;
-      margin-bottom: 24px;
-    }
-    h3 {
-      color: var(--letter-color-4);
-      font-size: 28px;
-    }
-  }
+  animation: ${appearFromLeft} 0.5s;
 
   h1 {
     color: var(--primary-color);
-    font-size: 8vh;
-    > strong {
-      font-weight: 500;
-      color: var(--title-color);
-    }
+    font-size: 80px;
   }
 
   form {
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    width: 100%;
+    margin: 40px 0;
+    width: 340px;
     text-align: center;
 
-    button {
-      height: 40px;
+    h1 {
+      color: var(--title-color);
+      margin-bottom: 24px;
+      font-size: 32px;
     }
 
-    h1 {
-      margin-bottom: 16px;
-    }
     a {
-      color: var(--letter-color-3);
+      color: var(--letter-color-5);
       display: block;
+      margin-top: 24px;
       text-decoration: none;
       transition: color 0.15s;
-      /* margin-top: 16px; */
 
       &:hover {
         color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
-  div {
+
+  > a {
+    color: var(--primary-color);
+    font-weight: 500;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.15s;
+    font-size: 24px;
+
     display: flex;
     align-items: center;
-    justify-content: center;
-    gap: 32px;
 
-    > a {
-      color: var(--primary-color);
-      display: block;
-      text-decoration: none;
-      transition: 0.3s;
-      font-size: 3vh;
-      font-weight: 500;
-      margin-top: 24px;
-
-      display: flex;
-      align-items: center;
-
-      p {
-        color: var(--letter-color-4);
-        transition: 0.3s;
-        &:hover {
-          color: var(--title-color);
-        }
-      }
-
-      svg {
-        margin-right: 16px;
-      }
-
-      &:hover {
-        color: var(--title-color);
-      }
-    }
-  }
-  @media (max-width: 800px) {
-    width: 90%;
-    /* margin-top: 56px; */
-    margin-top: 104px;
-
-    h1 {
-      color: var(--primary-color);
-      font-size: 40px;
-      font-weight: 500;
-      padding: 16px 16px;
-      background: var(--background-color);
-      border-radius: 8px;
-      width: 60%;
-      text-align: center;
-      margin-bottom: 16px;
-
-      > strong {
-        font-weight: 500;
-        color: var(--title-color);
-      }
+    svg {
+      margin-right: 16px;
     }
 
-    div {
-      &:nth-child(2) {
-        display: none;
-        /* text-align: center;
-        width: 60%;
-        margin: 0 auto;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: var(--header-background-color);
-        border-radius: 8px;
-      }
-      h1,
-      h3 {
-        padding: 0;
-        font-size: 24px;
-        font-weight: bold;
-        color: var(--title-color);
-        background: var(--header-background-color);
-      }
-      h1 {
-        padding-top: 24px; */
-      }
-      h2 {
-        display: none;
-        /* color: var(--primary-color); */
-      }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
     }
-    /* div:nth-child(4) {
-      h2 {
-        color: var(--background-color);
-        font-size: 20px;
-        padding: auto 0;
-        margin-bottom: 16px;
-      }
-    } */
   }
 `;
 
 export const Background = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  margin: auto;
-  z-index: -1;
   flex: 1;
-  background: url(${signUpBackgroundImg}) no-repeat center;
+  background: url(${signInBackgroundImg}) no-repeat center;
   background-size: cover;
-`;
-
-export const QuestionTitle = styled.h2`
-  color: var(--title-color);
-  font-size: 32px;
-  margin: 32px auto;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 16px;
-
-  > strong {
-    font-size: 40px;
-    color: var(--primary-color);
-  }
 `;
