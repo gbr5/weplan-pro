d73305e4ec836611bb6dbc77996d438f938952bc
GuyBR <gbrobbe@gmail.com>
2021-04-08 00:12:52 -0300

    S_216-PRO: Styles

--- src/hooks/homeController.tsx
+++ src/hooks/homeController.tsx
@@ -33,14 +33,14 @@ const HomeControllerProvider: React.FC = ({ children }) => {
 
   const selectPage = useCallback(
     async (page: string) => {
+      setSelectedPage(page);
+      localStorage.setItem('@WP-PRO:current-page', page);
       const trimmedCardName = selectedCard.name
         .toLowerCase()
         .replace(/ /g, '-');
-      localStorage.setItem('@WP-PRO:current-page', page);
       page === 'Card' && history.push(`/card/${trimmedCardName}`);
       page === 'Comercial' && history.push(`/funnel/comercial`);
       page === 'Settings' && history.push(`/settings`);
-      setSelectedPage(page);
     },
     [history, selectedCard],
   );
