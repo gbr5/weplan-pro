c005aa1792a2cc21378214dffb66fb13b6eb9d00
GuyBR <gbrobbe@gmail.com>
2021-04-08 00:18:47 -0300

    S_216-PRO: Styles

--- src/pages/SupplierDashboard/index.tsx
+++ src/pages/SupplierDashboard/index.tsx
@@ -3,17 +3,22 @@ import React from 'react';
 import { Container, Content } from './styles';
 
 import Header from '../../components/Header';
+import KanbanDashboard from '../../components/KabanDashboard';
 import TaskDashboard from '../../components/TaskDashboard';
+import CardPage from '../../components/CardPage';
 import ContactPageDashboard from '../../components/ContactPageComponents/ContactPageDashboard';
 import FormDashboard from '../../components/FormComponents/FormDashboard';
 import HomeDashboard from '../../components/HomeComponents/HomeDashBoard';
 import CompanyContactDashboard from '../../components/CompanyContactComponents/CompanyContactDashboard';
 import CustomerServiceOrderDashboard from '../../components/CustomerServiceOrderDashboard';
 import { useHomeController } from '../../hooks/homeController';
+import { useFunnel } from '../../hooks/funnel';
 import MenuButton from '../../components/MenuButton';
+import SettingsPage from '../SettingsPage';
 
 const SupplierDashboard: React.FC = () => {
   const { selectedPage } = useHomeController();
+  const { selectedFunnel, funnels } = useFunnel();
 
   return (
     <Container>
@@ -23,10 +28,15 @@ const SupplierDashboard: React.FC = () => {
         {selectedPage === 'Tasks' && <TaskDashboard />}
         {selectedPage === 'E-Links' && <ContactPageDashboard />}
         {selectedPage === 'Forms' && <FormDashboard />}
-
+        {funnels.length > 0 &&
+          selectedFunnel &&
+          selectedFunnel.id &&
+          selectedPage === 'Comercial' && <KanbanDashboard />}
+        {selectedPage === 'Card' && <CardPage />}
         {selectedPage === 'Contacts' && <CompanyContactDashboard />}
 
         {selectedPage === 'Home' && <HomeDashboard />}
+        {selectedPage === 'Settings' && <SettingsPage />}
         {selectedPage === 'ComercialOrders' && (
           <CustomerServiceOrderDashboard />
         )}
