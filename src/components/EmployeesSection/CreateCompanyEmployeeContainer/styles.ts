ca2f0ef4e16b58180ab8667fdba43219412bd797
GuyBR <gbrobbe@gmail.com>
2021-04-11 03:46:49 -0300

    S_224:PRO-CreateCompanyEmployee Layout

--- src/components/EmployeesSection/CreateCompanyEmployeeContainer/styles.ts
+++ src/components/EmployeesSection/CreateCompanyEmployeeContainer/styles.ts
@@ -7,12 +7,43 @@ export const Container = styled.div`
   justify-content: stretch;
   width: 100%;
   height: 100%;
-  padding: 8px;
+  padding: 1rem;
   padding-top: 3rem;
   overflow-y: scroll;
 
   background: var(--background-color);
-  box-shadow: var(--box-shadow);
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.4);
+  border-radius: 8px;
+
+  z-index: 12;
+  position: fixed;
+  top: 20%;
+  left: 20%;
+  height: 60%;
+  width: 60%;
+
+  @media (max-width: 700px) {
+    top: 5%;
+    left: 5%;
+    height: 90%;
+    width: 90%;
+  }
+
+  > header {
+    width: 100%;
+
+    > button {
+      position: absolute;
+      top: 8px;
+      right: 8px;
+      background: transparent;
+      border: none;
+      color: var(--red-color);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
 
   > h1 {
     font-size: 24px;
--- src/components/EmployeesSection/styles.ts
+++ src/components/EmployeesSection/styles.ts
@@ -4,3 +4,33 @@ export const Container = styled.div`
   display: block;
   padding-top: 5rem;
 `;
+
+export const SubContainer = styled.div`
+  display: block;
+  padding: 2rem;
+
+  @media (max-width: 700px) {
+    padding: 0.5rem;
+  }
+
+  > span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin-bottom: 1rem;
+
+    > h1 {
+      margin-right: 1rem;
+    }
+
+    > button {
+      margin-left: 1rem;
+      border: none;
+      background: var(--secondary-color);
+      padding: 0.5rem;
+      border-radius: 8px;
+      box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.2);
+    }
+  }
+`;
