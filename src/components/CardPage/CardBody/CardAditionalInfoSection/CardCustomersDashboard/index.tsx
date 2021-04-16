60cee0805bbf05c164c48036efa5cc91925443fd
GuyBR <gbrobbe@gmail.com>
2021-04-16 00:25:10 -0300

    S_242:PRO-CustomerDashboard On MainCardPage

--- src/components/CardPage/CardBody/CardAditionalInfoSection/CardCustomersDashboard/index.tsx
+++ src/components/CardPage/CardBody/CardAditionalInfoSection/CardCustomersDashboard/index.tsx
@@ -1,27 +1,78 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { Container, CustomerContainer } from './styles';
+import { MdAdd } from 'react-icons/md';
 import { useStageCard } from '../../../../../hooks/stageCard';
-import Button from '../../../../Button';
+import { useCompanyContact } from '../../../../../hooks/companyContacts';
+import ICompanyContactDTO from '../../../../../dtos/ICompanyContactDTO';
+import ContactWindow from '../../../../CompanyContactComponents/ContactWindow';
+import SelectCustomer from '../../../../GeneralComponents/SelectCustomer';
+
+import {
+  Container,
+  CustomerContainer,
+  ContainerHeader,
+  CustomerButton,
+} from './styles';
 
 const CardCustomersDashboard: React.FC = () => {
+  const iconsize = 40;
   const { cardCustomers } = useStageCard();
+  const { selectContact, selectedContact } = useCompanyContact();
+  const [contactWindow, setContactWindow] = useState(false);
+  const [addCustomerToCardWindow, setAddCustomerToCardWindow] = useState(false);
+
+  const handleOpenContactWindow = useCallback(
+    (e: ICompanyContactDTO) => {
+      selectContact(e);
+      setContactWindow(true);
+    },
+    [selectContact],
+  );
+  const handleOpenAddCustomerToCardWindow = useCallback(() => {
+    setAddCustomerToCardWindow(true);
+  }, []);
+  const handleCloseContactWindow = useCallback(() => {
+    setContactWindow(false);
+    selectContact({} as ICompanyContactDTO);
+  }, [selectContact]);
+  const handleCloseAddContactToCardWindow = useCallback(() => {
+    setAddCustomerToCardWindow(false);
+    if (selectedContact && selectedContact.id) {
+      setContactWindow(true);
+    }
+  }, [selectedContact]);
 
   return (
-    <Container>
-      <h2>Clientes</h2>
-
-      <CustomerContainer>
-        {cardCustomers.length > 0 &&
-          cardCustomers.map(customer => {
-            return (
-              <Button key={customer.id} type="button">
-                {customer.customer.name}
-              </Button>
-            );
-          })}
-      </CustomerContainer>
-    </Container>
+    <>
+      {contactWindow && selectedContact && selectedContact.id && (
+        <ContactWindow closeWindow={handleCloseContactWindow} />
+      )}
+      {addCustomerToCardWindow && (
+        <SelectCustomer closeWindow={handleCloseAddContactToCardWindow} />
+      )}
+      <Container>
+        <ContainerHeader>
+          <h2>Clientes</h2>
+          <button type="button" onClick={handleOpenAddCustomerToCardWindow}>
+            <MdAdd size={iconsize} />
+          </button>
+        </ContainerHeader>
+        <CustomerContainer>
+          {cardCustomers.length > 0 &&
+            cardCustomers.map(customer => {
+              return (
+                <CustomerButton
+                  onClick={() => handleOpenContactWindow(customer.customer)}
+                  key={customer.id}
+                  type="button"
+                >
+                  {customer.customer.name}
+                </CustomerButton>
+              );
+            })}
+        </CustomerContainer>
+      </Container>
+    </>
   );
 };
 
--- src/components/CardPage/CardBody/CardAditionalInfoSection/CardCustomersDashboard/styles.ts
+++ src/components/CardPage/CardBody/CardAditionalInfoSection/CardCustomersDashboard/styles.ts
@@ -6,13 +6,55 @@ export const Container = styled.div`
   flex-direction: column;
   height: 100%;
   /* box-sizing: border-box; */
-  max-width: 100vw;
+  width: 100%;
   text-align: center;
   overflow-y: scroll;
 
   h2 {
+    width: 90%;
+    margin: 1rem auto;
+    border-bottom: 2px solid var(--secondary-color);
+    padding: 0.5rem;
+  }
+`;
+
+export const ContainerHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 90%;
+  text-align: center;
+  position: relative;
+  margin: 1rem auto;
+
+  > h2 {
     width: 100%;
     margin: 0 auto;
+    border-bottom: 2px solid var(--header-primary);
+  }
+
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: var(--header-primary);
+    color: var(--letter-color-5);
+    border-radius: 50%;
+    border: none;
+    /* border: 1px solid var(--letter-color-5); */
+    box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.4);
+
+    transition: 0.5s;
+
+    &:hover {
+      box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.6);
+    }
   }
 `;
 
@@ -22,4 +64,25 @@ export const CustomerContainer = styled.div`
   overflow-y: scroll;
   width: 100%;
   padding: 0.5rem;
+`;
+
+export const CustomerButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 1rem;
+  background: var(--secondary-color);
+  color: var(--letter-color-5);
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  border: none;
+  border-radius: 6px;
+  font-size: 1.3rem;
+  font-weight: 500;
+  text-transform: capitalize;
+  margin: 1rem 0;
+
+  &:hover {
+    box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.3);
+  }
 `;
