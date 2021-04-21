ed3bc29711db65fb3f6d4adb2e43f3b5bded2a71
GuyBR <gbrobbe@gmail.com>
2021-04-21 06:01:28 -0300

    S_255-FixSortTask_256-FixUpdateIsActive'Delete'Task

--- src/components/TaskDashboard/Task/TaskDeleteButton/index.tsx
+++ src/components/TaskDashboard/Task/TaskDeleteButton/index.tsx
@@ -1,26 +1,25 @@
 import React, { useCallback, useState } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
+import ITaskDTO from '../../../../dtos/ITaskDTO';
 import { useCheckList } from '../../../../hooks/checkList';
 import ConfirmationWindow from '../../../GeneralComponents/ConfirmationWindow';
 
 import { Container } from './styles';
 
 interface IProps {
+  task: ITaskDTO;
   update: Function;
 }
 
-const TaskDeleteButton: React.FC<IProps> = ({ update }) => {
-  const { selectedTask, updateTask } = useCheckList();
+const TaskDeleteButton: React.FC<IProps> = ({ update, task }) => {
+  const { deleteTask } = useCheckList();
   const [deleteConfirmation, setDeleteConfirmation] = useState(false);
 
   const handleDeleteTask = useCallback(async () => {
-    await updateTask({
-      ...selectedTask,
-      isActive: false,
-    });
+    await deleteTask(task.id);
     setDeleteConfirmation(false);
     update();
-  }, [updateTask, update, selectedTask]);
+  }, [deleteTask, update, task]);
 
   const handleDeleteConfirmation = useCallback(async (e: boolean) => {
     return setDeleteConfirmation(e);
@@ -33,7 +32,7 @@ const TaskDeleteButton: React.FC<IProps> = ({ update }) => {
           closeWindow={() => handleDeleteConfirmation(false)}
           firstButtonFunction={handleDeleteTask}
           firstButtonLabel="Deletar"
-          message="Deseja mesmo deletar a tarefa"
+          message="Deseja deletar a tarefa?"
           secondButtonFunction={() => handleDeleteConfirmation(false)}
           secondButtonLabel="Não Deletar"
           zIndex={15}
